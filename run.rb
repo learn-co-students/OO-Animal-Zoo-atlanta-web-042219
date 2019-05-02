14a820aaa9b540fc25aa7df9245b756f9694f0ce
tloveATL <tlove.weaver@gmail.com>
2019-05-02 18:08:10 -0400

    done

--- run.rb
+++ run.rb
@@ -3,8 +3,12 @@ require_relative "lib/Zoo.rb"
 require 'pry'
 
 
-#Test your code here
+fauna = Zoo.new("Fauna Field", "Middle Earth")
+critter = Zoo.new("Critter Canyon", "Narnia")
 
+unicorn = Animal.new("Unicorn", 118, "Longhorn", fauna)
+phoenix = Animal.new("Phoenix", 1086, "Firebird", critter)
+phoenix2 = Animal.new("Phoenix", 1086, "Firebird", critter)
 
 binding.pry
 puts "done"
