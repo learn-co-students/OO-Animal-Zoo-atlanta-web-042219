9008094ce55fc7cf46d773be492302d5146bef52
Will Jones <willjones2010@gmail.com>
2019-05-01 23:59:32 -0400

    final

--- run.rb
+++ run.rb
@@ -4,6 +4,9 @@ require 'pry'
 
 
 #Test your code here
+zoo= Zoo.new('Happytown', 'Atlanta')
+animal_1 = Animal.new('monkey', 1000, 'Harambe', 'Happytown')
+animal_2 = Animal.new('dog', 3000, 'Biggy', 'Happytown')
 
 
 binding.pry
