5ffe19e820845ddf67f8b8095c67856605bac3d9
tunkhine126 <tunkhine126@gmail.com>
2019-05-02 13:41:13 -0400

    Done

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,22 @@
+require 'pry'
 class Animal
 
-end
+  attr_accessor :species, :weight, :nickname
+
+  @@animals = []
+
+  def initialize(species, weight=0, nickname)
+    @species = species
+    @weight = weight
+    @nickname = nickname
+    @@animals << self
+  end
+
+  def self.all
+    @@animals
+  end
+
+  def self.find_by_species(species)
+    self.all.select { |animal| animal.species == species}
+    end
+  end
